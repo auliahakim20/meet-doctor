1affe1615a157d3f792dbfb24753108073244a01
auliahakim20 <auliahakim5@gmail.com>
2026-09-07 23:19:13 +0700

    finish basic model

--- app/Models/Operational/Doctor.php
+++ app/Models/Operational/Doctor.php
@@ -4,8 +4,28 @@ namespace App\Models\Operational;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Doctor extends Model
 {
-    use HasFactory;
+    //use HasFactory;
+    use SoftDeletes;
+
+    public $table = 'doctor';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'name',
+        'specialist_id',
+        'photo',
+        'fee',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
 }
